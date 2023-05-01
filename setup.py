3f8a2fa4e144de5acc881c3e4475f88e2764f66d
tibeggs <tibeggs@gmail.com>
2023-05-01 12:20:15 -0400

    remove yum install

--- setup.py
+++ setup.py
@@ -1,22 +1,4 @@
-from setuptools import setup, find_packages, Command
-import subprocess
-
-class YumLocalMysql(Command):
-    """A Yum Installation of Mysql Server"""
-
-    description = 'A Yum Installation of Mysql Server'
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        subprocess.run(['sudo', 'yum', 'install', 'mysql-server*8.0.28*'])
-        subprocess.run(['sudo', 'yum', 'install', 'mysql*8.0.28*'])
-
+from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
@@ -31,7 +13,4 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3.8',
     ],
-    cmdclass={
-        'YumLocalMysql': YumLocalMysql,
-    }
 )
