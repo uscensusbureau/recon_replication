cd18ba94b34ac3bba8a72ed928f575810f08d300
tibeggs <tibeggs@gmail.com>
2023-05-01 11:11:54 -0400

    Create setup.py
    add setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages, Command
+import subprocess
+
+class YumLocalMysql(Command):
+    """A Yum Installation of Mysql Server"""
+
+    description = 'A Yum Installation of Mysql Server'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        # Run some additional setup steps here
+        print('Running my custom command...')
+        subprocess.run(['sudo', 'yum', 'install', 'mysql-server'])
+        subprocess.run(['sudo', 'yum', 'install', 'mysql'])
+
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='recon_replication',
+    version='1.0',
+    description='A replication package that reconstructs microdata from a subset of 2010 Census Summary File 1 table',
+    url='https://github.com/uscensusbureau/recon_replication',
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+    ],
+    cmdclass={
+        'YumLocalMysql': YumLocalMysql,
+    }
+)
