39f7e2b294edf5abb64b0144f75d10066f215bb1
tibeggs <tibeggs@gmail.com>
2023-05-03 19:44:19 -0400

    Remvoe uneed vars

--- SINGLE_STATE_EXAMPLE.sh
+++ SINGLE_STATE_EXAMPLE.sh
@@ -3,20 +3,18 @@
 # Example on how to run recon on a single state. This example uses Alabama.
 
 STATE="al"
-FIPST=01
 FIPCOUNTY=073
 FIPTRACT=005103
-TRACT=01073005103
 REIDENT="SINGLE_EXAMPLE"
 PYTHON="python3"
 
-$(./dbrtool.py --env);
+$(./dbrtool.py --env) ;
 
-./dbrtool.py --reident $REIDENT --register;
+./dbrtool.py --reident $REIDENT --register ;
 
-python3 s0_download_data.py --reident $REIDENT $STATE;
+python3 s0_download_data.py --reident $REIDENT $STATE ;
 
-./dbrtool.py --reident $REIDENT --step1 --step2 --stusab $STATE;
+./dbrtool.py --reident $REIDENT --step1 --step2 --stusab $STATE ;
 
 $PYTHON ./s3_pandas_synth_lp_files.py --reident $REIDENT $STATE $FIPCOUNTY $FIPTRACT ;
 
