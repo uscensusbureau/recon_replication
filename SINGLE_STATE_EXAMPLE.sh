557ff9938a1edd61199f02758fe66d3ba26fcfb4
tibeggs <tibeggs@gmail.com>
2023-05-03 14:32:38 -0400

    draft bypass dbrtool

--- SINGLE_STATE_EXAMPLE.sh
+++ SINGLE_STATE_EXAMPLE.sh
@@ -3,7 +3,12 @@
 # Example on how to run recon on a single state. This example uses Alabama.
 
 STATE="al"
+FIPST=01
+FIPCOUNTY=073
+FIPTRACT=005103
+TRACT=01073005103
 REIDENT="SINGLE_EXAMPLE"
+PYTHON="python3"
 
 $(./dbrtool.py --env);
 
@@ -13,4 +18,8 @@ python3 s0_download_data.py --reident $REIDENT $STATE;
 
 ./dbrtool.py --reident $REIDENT --step1 --step2 --stusab $STATE;
 
-./dbrtool.py --reident $REIDENT --stusab $STATE --runbg --launch_all --step5 --step6;
+$PYTHON ./s3_pandas_synth_lp_files.py --reident $REIDENT --stusab $STATE --tract $FIPTRACT --st $FIPCOUNTY ;
+
+$PYTHON ./s4_run_gurobi.py --reident $REIDENT --stusab $STATE --tract $FIPTRACT --st $FIPCOUNTY ;
+
+$PYTHON ./s5_make_microdata.py --reident $REIDENT --stusab $STATE --tract $FIPTRACT --st $FIPCOUNTY ;
