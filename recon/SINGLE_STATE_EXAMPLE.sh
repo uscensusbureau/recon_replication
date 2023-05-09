7437a8bfc1c77bdca76d28cb156aa2a9616878fe
tibeggs <tibeggs@gmail.com>
2023-05-09 08:19:11 -0400

    add j1-j2 options

--- recon/SINGLE_STATE_EXAMPLE.sh
+++ recon/SINGLE_STATE_EXAMPLE.sh
@@ -7,6 +7,10 @@ FIPCOUNTY=073
 FIPTRACT=005103
 REIDENT="SINGLE_EXAMPLE"
 PYTHON="python3"
+S3_J1=1
+S3_J2=4
+S4_J1=1
+S4_J2=16
 
 $(./dbrtool.py --env) ;
 
@@ -16,6 +20,6 @@ python3 s0_download_data.py --reident $REIDENT $STATE ;
 
 ./dbrtool.py --reident $REIDENT --step1 --step2 --stusab $STATE ;
 
-$PYTHON ./s3_pandas_synth_lp_files.py --reident $REIDENT $STATE $FIPCOUNTY $FIPTRACT ;
+$PYTHON ./s3_pandas_synth_lp_files.py --j1 $S3_J1 --j2 $S3_J2 --reident $REIDENT $STATE $FIPCOUNTY $FIPTRACT ;
 
-$PYTHON ./s4_run_gurobi.py --reident $REIDENT $STATE $FIPCOUNTY $FIPTRACT ;
+$PYTHON ./s4_run_gurobi.py --j1 $S4_J1 --j2 $S4_J2 --reident $REIDENT $STATE $FIPCOUNTY $FIPTRACT ;
